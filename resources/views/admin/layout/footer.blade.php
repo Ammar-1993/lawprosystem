fcc62495275f72ad32bf9f4ab04f0b9443c5914c
Ammar-1993 <ammaralnggar@gmail.com>
2026-09-01 03:34:05 +0300

    feat: implement responsive RTL-compatible footer component with branding and versioning

--- resources/views/admin/layout/footer.blade.php
+++ resources/views/admin/layout/footer.blade.php
@@ -1,6 +1,19 @@
-<footer>
+{{-- footer.blade.php --}}
+<footer class="lp-footer" role="contentinfo">
+    <div class="lp-footer__inner">
 
-    <div class="clearfix">
-        <h4 style="text-align: center">{{ __('frontend.footer.footer_text') }}</h4>
+        {{-- Left / Start: branding --}}
+        <div class="lp-footer__brand">
+            <i class="fa fa-balance-scale" aria-hidden="true"></i>
+            <span>{{ __('frontend.footer.footer_text') }}</span>
+        </div>
+
+        {{-- Right / End: version pill --}}
+        <div class="lp-footer__meta">
+            <span class="lp-footer__version">v1.0</span>
+            <span class="lp-footer__dot" aria-hidden="true">·</span>
+            <span>{{ date('Y') }}</span>
+        </div>
+
     </div>
 </footer>
