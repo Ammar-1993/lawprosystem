2b888279c74f9fa9fe284743a8be2fd6b436b49c
Ammar-1993 <ammaralnggar@gmail.com>
2026-09-07 09:28:37 +0300

    Phase 2 Batch 3 Prompt 1: Refactor task list UI

--- resources/views/admin/task/task.blade.php
+++ resources/views/admin/task/task.blade.php
@@ -15,11 +15,11 @@
 
         <div class="row">
             <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
+                <div class="x_panel lp-panel">
 
                     <div class="x_content">
 
-                        <table id="clientDataTable" class="table" data-url="{{ route('task.list') }}">
+                        <table id="clientDataTable" class="table lp-table" data-url="{{ route('task.list') }}">
                             <thead>
                                 <tr>
                                     <th>{{ __('frontend.task.no') }}</th>
