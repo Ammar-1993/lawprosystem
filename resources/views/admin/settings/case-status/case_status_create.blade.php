5161c1d38e3dedaa88557fdf03cf70f8c42cd515
Ammar-1993 <ammaralnggar@gmail.com>
2026-09-08 03:27:23 +0300

    Phase 2 Batch 4 Prompt 3: Refactor case-status modal UI

--- resources/views/admin/settings/case-status/case_status_create.blade.php
+++ resources/views/admin/settings/case-status/case_status_create.blade.php
@@ -19,9 +19,9 @@
                     <div class="row">
 
 
-                        <div class="col-md-12 col-sm-12 col-xs-12 form-group">
+                        <div class="col-md-12 col-sm-12 col-xs-12 form-group lp-form-group">
                             <label for="case_subtype">{{__('frontend.case_status')}}<span class="text-danger">*</span></label>
-                            <input type="text" placeholder="" class="form-control" id="case_status" name="case_status">
+                            <input type="text" placeholder="" class="form-control lp-input" id="case_status" name="case_status">
                         </div>
                     </div>
 
@@ -29,10 +29,10 @@
                 </div>
 
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-dismiss="modal"><i
+                    <button type="button" class="btn btn-danger lp-btn lp-btn-danger" data-dismiss="modal"><i
                             class="ik ik-x"></i>{{__('frontend.close')}}
                     </button>
-                    <button type="submit" class="btn btn-success shadow"><i class="fa fa-save   ik ik-check-circle"
+                    <button type="submit" class="btn btn-success lp-btn lp-btn-primary shadow"><i class="fa fa-save   ik ik-check-circle"
                                                                             id="cl">
                         </i> {{__('frontend.save')}}
                     </button>
--- resources/views/admin/settings/case-status/case_status_edit.blade.php
+++ resources/views/admin/settings/case-status/case_status_edit.blade.php
@@ -21,10 +21,10 @@
                     <div class="row">
 
 
-                        <div class="col-md-12 col-sm-12 col-xs-12 form-group">
+                        <div class="col-md-12 col-sm-12 col-xs-12 form-group lp-form-group">
                             <label for="case_subtype">{{__('frontend.case_status')}}<span class="text-danger">*</span></label>
                             <input type="text" value="{{ $CaseStatus->case_status_name ?? '' }}" placeholder=""
-                                   class="form-control" id="case_status" name="case_status">
+                                   class="form-control lp-input" id="case_status" name="case_status">
                         </div>
                     </div>
 
@@ -32,10 +32,10 @@
                 </div>
 
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-dismiss="modal"><i
+                    <button type="button" class="btn btn-danger lp-btn lp-btn-danger" data-dismiss="modal"><i
                             class="ik ik-x"></i>{{__('frontend.close')}}
                     </button>
-                    <button type="submit" class="btn btn-success shadow"><i class="fa fa-save   ik ik-check-circle"
+                    <button type="submit" class="btn btn-success lp-btn lp-btn-primary shadow"><i class="fa fa-save   ik ik-check-circle"
                                                                             id="cl">
                         </i> {{__('frontend.save')}}
                     </button>
