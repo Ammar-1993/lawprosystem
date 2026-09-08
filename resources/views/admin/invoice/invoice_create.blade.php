091b6670b1a96824e7fc386940c275f4bf4fb86c
Ammar-1993 <ammaralnggar@gmail.com>
2026-09-08 23:38:03 +0300

    Phase 2 Batch 5b Prompt 2: Refactor invoice create/edit forms UI

--- resources/views/admin/invoice/invoice_create.blade.php
+++ resources/views/admin/invoice/invoice_create.blade.php
@@ -16,13 +16,13 @@
                 <div class="form-group pull-right top_search">
 
 
-                    <a href="{{ url('admin/invoice') }}" class="btn btn-primary">{{__('frontend.back')}}</a>
+                    <a href="{{ url('admin/invoice') }}" class="btn btn-primary lp-btn lp-btn-primary">{{__('frontend.back')}}</a>
 
 
                 </div>
             </div>
         </div>
-        <div class="x_panel">
+        <div class="x_panel lp-panel">
 
             <div class="x_content">
 
@@ -51,9 +51,9 @@
 
 
                             <div class="row">
-                                <div class="col-md-12 form-group ">
+                                <div class="col-md-12 form-group lp-form-group">
                                     <label for="vendor">{{__('frontend.clientf')}}<span class="text-danger">*</span></label>
-                                    <select class="form-control client_id " name="client_id" id="client_id"
+                                    <select class="form-control lp-input client_id " name="client_id" id="client_id"
                                             onchange="getClientDetail(this.value);">
                                         <option value="">{{__('frontend.select_client')}}</option>
                                         @foreach($client_list as $list)
@@ -83,7 +83,7 @@
                                 <label class="control-label col-md-5 col-sm-3 col-xs-12">{{__('frontend.invoice_date')}} <span
                                         class="text-danger">*</span></label>
                                 <div class="col-md-7 col-sm-9 col-xs-12">
-                                    <input type="text" class="form-control inc_Date" id="inc_Date" name="inc_Date"
+                                    <input type="text" class="form-control lp-input inc_Date" id="inc_Date" name="inc_Date"
                                            readonly="">
                                 </div>
                             </div>
@@ -92,7 +92,7 @@
                                 <label class="control-label col-md-5 col-sm-3 col-xs-12">{{__('frontend.invoice_due_date')}} <span
                                         class="text-danger">*</span></label>
                                 <div class="col-md-7 col-sm-9 col-xs-12">
-                                    <input type="text" class="form-control due_Date" id="due_Date" name="due_Date"
+                                    <input type="text" class="form-control lp-input due_Date" id="due_Date" name="due_Date"
                                            readonly="">
                                 </div>
                             </div>
@@ -107,7 +107,7 @@
                     <div class="row">
                         <div class="col-sm-12">
                             <div class="table-responsive">
-                                <table class="table tableInv" id="purchaseInvoice" data-repeater-list="invoice_items">
+                                <table class="table lp-table tableInv" id="purchaseInvoice" data-repeater-list="invoice_items">
                                     <thead class="thead-inverse">
                                     <tr class="tbl_header_color dynamicRows">
 
@@ -141,7 +141,7 @@
                                     <tr data-repeater-item>
 
                                         <th width="30%" class="text-center">
-                                            <select class="form-control sel services" name="services" id="services"
+                                            <select class="form-control lp-input sel services" name="services" id="services"
                                                     data-rule-required="true">
                                                 <option MyServiceAmount="0.00" value="">Select Services</option>
                                                 @foreach($service_lists as $service)
@@ -152,28 +152,28 @@
                                         </th>
 
                                         <th width="" class="text-center">
-                                            <input type="text" class="form-control" id="description" name="description">
+                                            <input type="text" class="form-control lp-input" id="description" name="description">
 
                                         </th>
 
                                         <th width="10%" class="text-center">
-                                            <input type="text" class="form-control qty" id="qty" name="qty"
+                                            <input type="text" class="form-control lp-input qty" id="qty" name="qty"
                                                    data-rule-required="true" maxlength="8"
                                                    onkeypress='return isNumber(event)'>
                                         </th>
                                         <th width="10%" class="text-center">
-                                            <input readonly="" type="text" class="form-control rate"
+                                            <input readonly="" type="text" class="form-control lp-input rate"
                                                    onkeypress='return isFloatsNumberKey(event)' id="rate" name="rate"
                                                    data-rule-required="true" maxlength="10">
                                         </th>
 
                                         <th width="10%" class="text-center">
-                                            <input type="text" class="form-control amount" id="amount" name="amount"
+                                            <input type="text" class="form-control lp-input amount" id="amount" name="amount"
                                                    data-rule-required="true" readonly=""></th>
                                         <th width="5%" class="text-center">
 
                                             <button type="button" data-repeater-delete type="button"
-                                                    class="btn btn-danger waves-effect waves-light"><i
+                                                    class="btn btn-danger lp-btn lp-btn-danger waves-effect waves-light"><i
                                                     class="fa fa-trash-o" aria-hidden="true"></i></button>
 
                                         </th>
@@ -190,7 +190,7 @@
                             </div>
                             <br>
                             <button data-repeater-create type="button" value="Add New"
-                                    class="btn btn-success waves-effect waves-light btn btn-success-edit" type="button">
+                                    class="btn btn-success lp-btn lp-btn-primary waves-effect waves-light btn-success-edit" type="button">
                                 <i class="fa fa-plus" aria-hidden="true"></i>&nbsp;{{__('frontend.add_more')}}
                             </button>
 
@@ -208,7 +208,7 @@
                                         <div class="form-group">
                                             <label class="discount_text">{{__('frontend.note')}}
                                             </label>
-                                            <textarea class="form-control" id="note" name="note" rows="4"></textarea>
+                                            <textarea class="form-control lp-input" id="note" name="note" rows="4"></textarea>
                                         </div>
                                     </div>
                                 </div>
@@ -218,7 +218,7 @@
                                             <th class="text-left expence-p-top-18 ">{{__('frontend.subtotal')}}</th>
                                             <td class="text-center">
                                                 <input type="text" name="subTotal"
-                                                       class="form-control expence-sub-total" id="subTotal"
+                                                       class="form-control lp-input expence-sub-total" id="subTotal"
                                                        readonly=""
                                                 >
                                             </td>
@@ -228,7 +228,7 @@
                                     <table class="table row-border dataTable no-footer" id="tab_logic_total">
                                         <tr>
                                             <th class="text-center">
-                                                <select id="tax" class="tax" name="tax" class="form-control">
+                                                <select id="tax" class="form-control lp-input tax" name="tax">
                                                     <option MyTax="" value="">Select Tax</option>
                                                     @foreach($tax as $t)
                                                         <option MyTax="{{ $t->per }}"
@@ -238,7 +238,7 @@
                                             </th>
                                             <td class="text-center">
                                                 <input type="text" value="" name="taxVal"
-                                                       class="form-control expence-sub-total"
+                                                       class="form-control lp-input expence-sub-total"
                                                        id="taxVal" readonly=""
                                                 >
                                             </td>
@@ -252,7 +252,7 @@
                                             <td class="text-center total-width-expence">
                                                 <input type="text" name="total"
 
-                                                       class="form-control total-width-expence-border " id="grandTotal"
+                                                       class="form-control lp-input total-width-expence-border" id="grandTotal"
                                                        readonly="">
                                             </td>
                                         </tr>
@@ -263,9 +263,9 @@
                             <div class="row">
                                 <div class="col-md-9"></div>
                                 <div class="col-md-3 text-center">
-                                    <a href="{{ url('admin/invoice') }}" class="btn btn-danger">{{__('frontend.cancel')}}</a>
+                                    <a href="{{ url('admin/invoice') }}" class="btn btn-danger lp-btn lp-btn-danger">{{__('frontend.cancel')}}</a>
 
-                                    <button type="submit" name="btn_add_offer" class="btn_add_offer btn btn-success"><i
+                                    <button type="submit" name="btn_add_offer" class="btn_add_offer btn btn-success lp-btn lp-btn-primary"><i
                                             class="fa fa-save" id="show_loader"></i>&nbsp;{{__('frontend.save')}}
                                     </button>
 
--- resources/views/admin/invoice/invoice_edit.blade.php
+++ resources/views/admin/invoice/invoice_edit.blade.php
@@ -16,13 +16,13 @@
                 <div class="form-group pull-right top_search">
 
 
-                    <a href="{{ url('admin/invoice') }}" class="btn btn-primary">{{__('frontend.back')}}</a>
+                    <a href="{{ url('admin/invoice') }}" class="btn btn-primary lp-btn lp-btn-primary">{{__('frontend.back')}}</a>
 
 
                 </div>
             </div>
         </div>
-        <div class="x_panel">
+        <div class="x_panel lp-panel">
 
             <div class="x_content">
 
@@ -51,9 +51,9 @@
 
 
                             <div class="row">
-                                <div class="col-md-12 form-group">
+                                <div class="col-md-12 form-group lp-form-group">
                                     <label for="vendor">{{__('frontend.clientf')}}<span class="text-danger">*</span></label>
-                                    <select class="form-control client_id" name="client_id" id="client_id"
+                                    <select class="form-control lp-input client_id" name="client_id" id="client_id"
                                         onchange="getClientDetail(this.value);">
                                         <option value="">{{__('frontend.select_client')}}</option>
                                         @foreach ($client_list as $list)
@@ -87,7 +87,7 @@
                                 <label class="control-label col-md-5 col-sm-3 col-xs-12">{{__('frontend.invoice_date')}} <span
                                         class="text-danger">*</span></label>
                                 <div class="col-md-7 col-sm-9 col-xs-12">
-                                    <input type="text" class="form-control inc_Date" id="inc_Date" name="inc_Date"
+                                    <input type="text" class="form-control lp-input inc_Date" id="inc_Date" name="inc_Date"
                                         readonly=""
                                         value="{{ date($date_format_laravel, strtotime($invoice->inv_date)) }}">
                                 </div>
@@ -97,7 +97,7 @@
                                 <label class="control-label col-md-5 col-sm-3 col-xs-12">{{__('frontend.invoice_due_date')}} <span
                                         class="text-danger">*</span></label>
                                 <div class="col-md-7 col-sm-9 col-xs-12">
-                                    <input type="text" class="form-control due_Date" id="due_Date" name="due_Date"
+                                    <input type="text" class="form-control lp-input due_Date" id="due_Date" name="due_Date"
                                         readonly=""
                                         value="{{ date($date_format_laravel, strtotime($invoice->due_date)) }}">
                                 </div>
@@ -113,7 +113,7 @@
                     <div class="row">
                         <div class="col-sm-12">
                             <div class="table-responsive">
-                                <table class="table tableInv" id="purchaseInvoice" data-repeater-list="invoice_items">
+                                <table class="table lp-table tableInv" id="purchaseInvoice" data-repeater-list="invoice_items">
                                     <thead class="thead-inverse">
                                         <tr class="tbl_header_color dynamicRows">
 
@@ -150,7 +150,7 @@
 
 
                                                 <th width="30%" class="text-center">
-                                                    <select class="form-control sel services" name="services"
+                                                    <select class="form-control lp-input sel services" name="services"
                                                         id="services" data-rule-required="true">
                                                         <option MyServiceAmount="0.00" value="">Select Services
                                                         </option>
@@ -166,32 +166,32 @@
                                                     <input type="hidden" class="form-control" id="id"
                                                         name="id" value="{{ $i->id ?? '' }}">
 
-                                                    <input type="text" class="form-control" id="description"
+                                                    <input type="text" class="form-control lp-input" id="description"
                                                         name="description" value="{{ $i->item_description ?? '' }}">
                                                 </th>
 
                                                 <th width="10%" class="text-center">
-                                                    <input type="text" class="form-control qty" id="qty"
+                                                    <input type="text" class="form-control lp-input qty" id="qty"
                                                         name="qty" value="{{ $i->iteam_qty ?? '' }}"
                                                         data-rule-required="true" maxlength="8"
                                                         onkeypress='return isNumber(event)'>
                                                 </th>
                                                 <th width="10%" class="text-center">
-                                                    <input readonly="" type="text" class="form-control rate"
+                                                    <input readonly="" type="text" class="form-control lp-input rate"
                                                         onkeypress='return isFloatsNumberKey(event)' id="rate"
                                                         name="rate" data-rule-required="true" maxlength="10"
                                                         value="{{ $i->item_rate ?? '' }}">
                                                 </th>
 
                                                 <th width="10%" class="text-center">
-                                                    <input type="text" class="form-control amount" id="amount"
+                                                    <input type="text" class="form-control lp-input amount" id="amount"
                                                         name="amount" data-rule-required="true" readonly=""
                                                         value="{{ $i->item_amount ?? '' }}">
                                                 </th>
                                                 <th width="5%" class="text-center">
 
                                                     <button type="button" data-repeater-delete type="button"
-                                                        class="btn btn-danger waves-effect waves-light btn_remove"><i
+                                                        class="btn btn-danger lp-btn lp-btn-danger waves-effect waves-light btn_remove"><i
                                                             class="fa fa-trash-o" aria-hidden="true"></i></button>
 
                                                 </th>
@@ -209,7 +209,7 @@
                             </div>
                             <br>
                             <button data-repeater-create type="button" value="Add New"
-                                class="btn btn-success waves-effect waves-light btn btn-success-edit" type="button">
+                                class="btn btn-success lp-btn lp-btn-primary waves-effect waves-light btn-success-edit" type="button">
                                 <i class="fa fa-plus" aria-hidden="true"></i>&nbsp;{{__('frontend.add_more')}}
                             </button>
 
@@ -227,7 +227,7 @@
                                         <div class="form-group">
                                             <label class="discount_text">{{__('frontend.note')}}
                                             </label>
-                                            <textarea class="form-control" id="note" name="note" rows="4">{{ $invoice->remarks ?? '' }}</textarea>
+                                            <textarea class="form-control lp-input" id="note" name="note" rows="4">{{ $invoice->remarks ?? '' }}</textarea>
                                         </div>
                                     </div>
                                 </div>
@@ -237,7 +237,7 @@
                                             <th class="text-left expence-p-top-18 ">{{__('frontend.subtotal')}}</th>
                                             <td class="text-center total-width-expence">
                                                 <input value="{{ $invoice->sub_total_amount ?? '' }}" type="text"
-                                                    name="subTotal" class="form-control expence-sub-total "
+                                                    name="subTotal" class="form-control lp-input expence-sub-total"
                                                     id="subTotal" readonly="">
                                             </td>
                                         </tr>
@@ -246,8 +246,7 @@
                                     <table class="table row-border dataTable no-footer" id="tab_logic_total">
                                         <tr>
                                             <th class="text-center">
-                                                <select id="tax" class="tax" name="tax"
-                                                    class="form-control">
+                                                <select id="tax" class="form-control lp-input tax" name="tax">
                                                     <option MyTax="" value="">Select Tax</option>
                                                     @foreach ($tax as $t)
                                                         <option MyTax="{{ $t->per }}" value="{{ $t->id }}"
@@ -259,7 +258,7 @@
                                             </th>
                                             <td class="text-center total-width-expence">
                                                 <input type="text" name="taxVal"
-                                                    class="form-control expence-sub-total" id="taxVal"
+                                                    class="form-control lp-input expence-sub-total" id="taxVal"
                                                     value="{{ $invoice->tax_amount ?? '' }}" readonly="">
                                             </td>
                                         </tr>
@@ -271,7 +270,7 @@
                                             <th class="text-left expence-p-top-18">{{__('frontend.total')}}</th>
                                             <td class="text-center total-width-expence">
                                                 <input type="text" name="total"
-                                                    class="form-control total-width-expence-border " id="grandTotal"
+                                                    class="form-control lp-input total-width-expence-border" id="grandTotal"
                                                     readonly="" value="{{ $invoice->total_amount ?? '' }}">
                                             </td>
                                         </tr>
@@ -282,8 +281,8 @@
                             <div class="row">
                                 <div class="col-md-9"></div>
                                 <div class="col-md-3 text-center">
-                                    <a href="{{ url('admin/invoice') }}" class="btn btn-danger">{{__('frontend.cancel')}}</a>
-                                    <button type="submit" name="btn_add_offer" class="btn_add_offer btn btn-success"><i
+                                    <a href="{{ url('admin/invoice') }}" class="btn btn-danger lp-btn lp-btn-danger">{{__('frontend.cancel')}}</a>
+                                    <button type="submit" name="btn_add_offer" class="btn_add_offer btn btn-success lp-btn lp-btn-primary"><i
                                             class="fa fa-save" id="show_loader"></i>&nbsp;{{__('frontend.save')}}
                                     </button>
 
