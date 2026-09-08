c6171532f1fe995e920dd90719f9d8e58060f51e
Ammar-1993 <ammaralnggar@gmail.com>
2026-09-08 03:16:11 +0300

    Phase 2 Batch 4 Prompt 2: Refactor settings list tables UI

--- resources/views/admin/settings/case-type/casetype.blade.php
+++ resources/views/admin/settings/case-type/casetype.blade.php
@@ -18,11 +18,11 @@
 
         <div class="row">
             <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
+                <div class="x_panel lp-panel">
 
                     <div class="x_content">
 
-                        <table id="tagDataTable" class="table" data-url="{{ route('cash.type.list') }}"
+                        <table id="tagDataTable" class="table lp-table" data-url="{{ route('cash.type.list') }}"
                                >
                             <thead>
                             <tr>
--- resources/views/admin/settings/judge/judge.blade.php
+++ resources/views/admin/settings/judge/judge.blade.php
@@ -18,11 +18,11 @@
 
         <div class="row">
             <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="x_panel">
+                <div class="x_panel lp-panel">
 
                     <div class="x_content">
 
-                        <table id="tagDataTable" class="table" data-url="{{ route('judge.list') }}">
+                        <table id="tagDataTable" class="table lp-table" data-url="{{ route('judge.list') }}">
                             <thead>
                                 <tr>
                                     <th width="5%">{{__('frontend.no')}}</th>
