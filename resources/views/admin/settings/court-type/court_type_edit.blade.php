bfdb022c55bd14492e71fe7aecb8b5852c84e1bc
Ammar-1993 <ammaralnggar@gmail.com>
2026-09-08 03:31:04 +0300

    Phase 2 Batch 4 Prompt 3: Refactor court-type modal UI

--- resources/views/admin/settings/court-type/court_type_edit.blade.php
+++ resources/views/admin/settings/court-type/court_type_edit.blade.php
@@ -22,9 +22,9 @@
                     <div class="row">
 
 
-          <div class="col-md-12 col-sm-12 col-xs-12 form-group">
+          <div class="col-md-12 col-sm-12 col-xs-12 form-group lp-form-group">
               <label for="case_subtype">{{__('frontend.court_type')}}<span class="text-danger">*</span></label>
-              <input type="text" placeholder="" class="form-control" value="{{ $court->court_type_name ?? '' }}" id="court_name" name="court_name">
+              <input type="text" placeholder="" class="form-control lp-input" value="{{ $court->court_type_name ?? '' }}" id="court_name" name="court_name">
           </div>
         </div>
 
@@ -33,9 +33,9 @@
                 </div>
 
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-dismiss="modal"><i
+                    <button type="button" class="btn btn-danger lp-btn lp-btn-danger" data-dismiss="modal"><i
                             class="ik ik-x"></i>{{__('frontend.close')}}</button>
-                    <button type="submit" class="btn btn-success shadow"><i class=" fa fa-save ik ik-check-circle" id="cl">
+                    <button type="submit" class="btn btn-success lp-btn lp-btn-primary shadow"><i class=" fa fa-save ik ik-check-circle" id="cl">
                         </i> {{__('frontend.save')}}</button>
                 </div>
 
