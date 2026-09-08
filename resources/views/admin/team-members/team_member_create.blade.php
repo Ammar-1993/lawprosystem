4a8c8290653aafeefc00425460695fdb0ec4f3a8
Ammar-1993 <ammaralnggar@gmail.com>
2026-09-08 04:09:43 +0300

    Phase 2 Batch 5a Prompt 2: Refactor team members create/edit UI

--- resources/views/admin/team-members/team_member_create.blade.php
+++ resources/views/admin/team-members/team_member_create.blade.php
@@ -20,7 +20,7 @@
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xs-12">
             @include('component.error')
-            <div class="x_panel">
+            <div class="x_panel lp-panel">
                 <div class="x_content">
                     <form id="add_user" name="add_user" role="form" method="POST"
                         action="{{ route('client_user.store') }}" enctype="multipart/form-data">
@@ -50,7 +50,7 @@
                                                     <input type="file" id="upload" name="image" data-src="">
 
                                                 </label>
-                                                <button type="button" class="btn btn-default"
+                                                <button type="button" class="btn btn-default lp-btn"
                                                     id="cancel_img">{{ __('frontend.cancel') }}
                                                 </button>
                                             </div>
@@ -61,88 +61,88 @@
 
                             <div class="col-md-8 col-sm-12 col-xs-12">
                                 <div class="row form-group">
-                                    <div class="col-md-6">
+                                    <div class="col-md-6 lp-form-group">
                                         <label for="f_name">{{ __('frontend.first_name') }}<span
                                                 class="text-danger">*</span></label>
                                         <input type="text" id="f_name" name="f_name" placeholder=""
-                                            class="form-control">
+                                            class="form-control lp-input">
                                     </div>
-                                    <div class="col-md-6">
+                                    <div class="col-md-6 lp-form-group">
                                         <label for="last_name">{{ __('frontend.last_name') }}<span
                                                 class="text-danger">*</span></label>
-                                        <input type="text" id="l_name" name="l_name" class="form-control">
+                                        <input type="text" id="l_name" name="l_name" class="form-control lp-input">
                                     </div>
                                 </div>
 
                                 <div class="row form-group">
-                                    <div class="col-md-6">
+                                    <div class="col-md-6 lp-form-group">
                                         <label for="email">{{ __('frontend.email_id') }}<span
                                                 class="text-danger">*</span></label>
-                                        <input type="text" id="email" name="email" class="form-control">
+                                        <input type="text" id="email" name="email" class="form-control lp-input">
                                     </div>
-                                    <div class="col-md-6">
+                                    <div class="col-md-6 lp-form-group">
                                         <label for="mobile">{{ __('frontend.mobile_no') }}<span
                                                 class="text-danger">*</span></label>
-                                        <input type="text" id="mobile" name="mobile" class="form-control"
+                                        <input type="text" id="mobile" name="mobile" class="form-control lp-input"
                                             maxlength="10">
                                     </div>
                                 </div>
 
                                 <div class="row form-group">
-                                    <div class="col-md-9">
+                                    <div class="col-md-9 lp-form-group">
                                         <label for="address">{{ __('frontend.address') }} <span
                                                 class="text-danger">*</span></label>
-                                        <input type="text" id="address" name="address" class="form-control">
+                                        <input type="text" id="address" name="address" class="form-control lp-input">
                                     </div>
-                                    <div class="col-md-3">
+                                    <div class="col-md-3 lp-form-group">
                                         <label for="zipcode">{{ __('frontend.zip_code') }}<span
                                                 class="text-danger">*</span></label>
-                                        <input type="text" id="zip_code" name="zip_code" class="form-control"
+                                        <input type="text" id="zip_code" name="zip_code" class="form-control lp-input"
                                             maxlength="">
                                     </div>
                                 </div>
                                 <div class="row form-group">
-                                    <div class="col-md-6">
+                                    <div class="col-md-6 lp-form-group">
                                         <label for="password">{{ __('frontend.password') }}<span
                                                 class="text-danger">*</span></label>
-                                        <input type="password" id="password" name="password" class="form-control"
+                                        <input type="password" id="password" name="password" class="form-control lp-input"
                                             autocomplete="off">
                                     </div>
-                                    <div class="col-md-6">
+                                    <div class="col-md-6 lp-form-group">
                                         <label for="cnm_password">{{ __('frontend.confirm_password') }}<span
                                                 class="text-danger">*</span></label>
                                         <input type="password" id="cnm_password" name="cnm_password"
-                                            class="form-control" autocomplete="off">
+                                            class="form-control lp-input" autocomplete="off">
                                     </div>
                                 </div>
 
                                 <div class="row form-group">
-                                    <div class="col-md-4">
+                                    <div class="col-md-4 lp-form-group">
                                         <label for="country">{{ __('frontend.country') }}<span
                                                 class="text-danger">*</span></label>
-                                        <select class="form-control select-change country-select2 selct2-width-100"
+                                        <select class="form-control lp-input select-change country-select2 selct2-width-100"
                                             name="country" id="country" data-url="{{ route('get.country') }}"
                                             data-clear="#city_id,#state">
                                             <option value="">{{ __('frontend.select_country') }}</option>
 
                                         </select>
                                     </div>
-                                    <div class="col-md-4">
+                                    <div class="col-md-4 lp-form-group">
                                         <label for="state">{{ __('frontend.state') }} <span
                                                 class="text-danger">*</span></label>
                                         <select id="state" name="state" data-url="{{ route('get.state') }}"
                                             data-target="#country" data-clear="#city_id"
-                                            class="form-control state-select2 select-change">
+                                            class="form-control lp-input state-select2 select-change">
                                             <option value="">{{ __('frontend.select_state') }}</option>
 
 
                                         </select>
                                     </div>
-                                    <div class="col-md-4">
+                                    <div class="col-md-4 lp-form-group">
                                         <label for="city">{{ __('frontend.city') }}<span
                                                 class="text-danger">*</span></label>
                                         <select id="city_id" name="city_id" data-url="{{ route('get.city') }}"
-                                            data-target="#state" class="form-control city-select2">
+                                            data-target="#state" class="form-control lp-input city-select2">
                                             <option value="">{{ __('frontend.select_city') }}</option>
 
 
@@ -152,10 +152,10 @@
                                 </div>
 
                                 <div class="row form-group">
-                                    <div class="col-md-4">
+                                    <div class="col-md-4 lp-form-group">
                                         <label for="Role">{{ __('frontend.role') }} <span
                                                 class="text-danger">*</span></label>
-                                        <select id="role" name="role" required class="form-control select2">
+                                        <select id="role" name="role" required class="form-control lp-input select2">
                                             <option value=""> {{ __('frontend.select_role') }}</option>
                                             @foreach ($roles as $roal)
                                                 <option value="{{ $roal->id }}">{{ $roal->slug }}</option>
@@ -171,10 +171,10 @@
                             <div class="form-group pull-right">
                                 <div class="col-md-12 col-sm-6 col-xs-12">
                                     <br>
-                                    <a class="btn btn-danger"
+                                    <a class="btn btn-danger lp-btn lp-btn-danger"
                                         href="{{ url('admin/client_user') }}">{{ __('frontend.cancel') }}</a>
 
-                                    <button type="submit" class="btn btn-success" id="upload-result"><i
+                                    <button type="submit" class="btn btn-success lp-btn lp-btn-primary" id="upload-result"><i
                                             class="fa fa-save" id="show_loader"></i>&nbsp;{{ __('frontend.save') }}
                                     </button>
                                 </div>
--- resources/views/admin/team-members/team_member_edit.blade.php
+++ resources/views/admin/team-members/team_member_edit.blade.php
@@ -21,7 +21,7 @@
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xs-12">
             @include('component.error')
-            <div class="x_panel">
+            <div class="x_panel lp-panel">
                 <div class="x_content">
                     <form id="add_user" name="add_user" role="form" method="POST" enctype="multipart/form-data"
                           action="{{route('client_user.update',$users->id)}}">
@@ -66,7 +66,7 @@
                                                     <input type="file" id="upload" name="image" data-src="">
 
                                                 </label>
-                                                <button type="button" class="btn btn-default" id="cancel_img">{{__('frontend.cancel')}}
+                                                <button type="button" class="btn btn-default lp-btn" id="cancel_img">{{__('frontend.cancel')}}
                                                 </button>
                                             </div>
                                         </div>
@@ -76,47 +76,47 @@
 
                             <div class="col-md-8 col-sm-12 col-xs-12">
                                 <div class="row form-group">
-                                    <div class="col-md-6">
+                                    <div class="col-md-6 lp-form-group">
                                         <label for="f_name">{{__('frontend.first_name')}}<span class="text-danger">*</span></label>
-                                        <input type="text" id="f_name" name="f_name" placeholder="" class="form-control"
+                                        <input type="text" id="f_name" name="f_name" placeholder="" class="form-control lp-input"
                                                value="{{ $users->first_name}}">
                                     </div>
-                                    <div class="col-md-6">
+                                    <div class="col-md-6 lp-form-group">
                                         <label for="last_name">{{__('frontend.last_name')}}<span class="text-danger">*</span></label>
-                                        <input type="text" id="l_name" name="l_name" class="form-control"
+                                        <input type="text" id="l_name" name="l_name" class="form-control lp-input"
                                                value="{{ $users->last_name}}">
                                     </div>
                                 </div>
 
                                 <div class="row form-group">
-                                    <div class="col-md-6">
+                                    <div class="col-md-6 lp-form-group">
                                         <label for="email">{{__('frontend.email_id')}}<span class="text-danger">*</span></label>
-                                        <input type="text" id="email" name="email" class="form-control"
+                                        <input type="text" id="email" name="email" class="form-control lp-input"
                                                value="{{ $users->email}}" readonly="">
                                     </div>
-                                    <div class="col-md-6">
+                                    <div class="col-md-6 lp-form-group">
                                         <label for="mobile">{{__('frontend.mobile_no')}}<span class="text-danger">*</span></label>
-                                        <input type="text" id="mobile" name="mobile" class="form-control" maxlength="10"
+                                        <input type="text" id="mobile" name="mobile" class="form-control lp-input" maxlength="10"
                                                value="{{ $users->mobile}}" readonly="">
                                     </div>
                                 </div>
 
                                 <div class="row form-group">
-                                    <div class="col-md-9">
+                                    <div class="col-md-9 lp-form-group">
                                         <label for="address">{{__('frontend.address')}} <span class="text-danger">*</span></label>
-                                        <input type="text" id="address" name="address" class="form-control"
+                                        <input type="text" id="address" name="address" class="form-control lp-input"
                                                value="{{ $users->address}}">
                                     </div>
-                                    <div class="col-md-3">
+                                    <div class="col-md-3 lp-form-group">
                                         <label for="zipcode">{{__('frontend.zip_code')}}<span class="text-danger">*</span></label>
-                                        <input type="text" id="zip_code" name="zip_code" class="form-control"
+                                        <input type="text" id="zip_code" name="zip_code" class="form-control lp-input"
                                                maxlength="" value="{{ $users->zipcode}}">
                                     </div>
                                 </div>
                                 <div class="row form-group">
-                                    <div class="col-md-4">
+                                    <div class="col-md-4 lp-form-group">
                                         <label for="country">{{__('frontend.country')}}<span class="text-danger">*</span></label>
-                                        <select class="form-control select-change country-select2 selct2-width-100"
+                                        <select class="form-control lp-input select-change country-select2 selct2-width-100"
                                                 name="country" id="country"
                                                 data-url="{{ route('get.country') }}"
                                                 data-clear="#city_id,#state"
@@ -129,14 +129,14 @@
 
                                         </select>
                                     </div>
-                                    <div class="col-md-4">
+                                    <div class="col-md-4 lp-form-group">
                                         <label for="state">{{__('frontend.state')}}<span class="text-danger">*</span></label>
                                         <select id="state" name="state"
 
                                                 data-url="{{ route('get.state') }}"
                                                 data-target="#country"
                                                 data-clear="#city_id"
-                                                class="form-control state-select2 select-change">
+                                                class="form-control lp-input state-select2 select-change">
                                             <option value="">{{__('frontend.select_state')}}</option>
                                             @if ($users->state)
                                                 <option value="{{ $users->state->id }}"
@@ -146,13 +146,13 @@
 
                                         </select>
                                     </div>
-                                    <div class="col-md-4">
+                                    <div class="col-md-4 lp-form-group">
                                         <label for="city">{{__('frontend.city')}}<span class="text-danger">*</span></label>
                                         <select id="city_id" name="city_id"
                                                 data-url="{{ route('get.city') }}"
                                                 data-target="#state"
 
-                                                class="form-control city-select2">
+                                                class="form-control lp-input city-select2">
                                             <option value="">{{__('frontend.select_city')}}</option>
                                             @if($users->city)
                                                 <option value="{{ $users->city->id }}"
@@ -166,9 +166,9 @@
                                 </div>
 
                                 <div class="row form-group">
-                                    <div class="col-md-4">
+                                    <div class="col-md-4 lp-form-group">
                                         <label for="Role">{{__('frontend.role')}}<span class="text-danger">*</span></label>
-                                        <select id="role" name="role" required class="form-control select2">
+                                        <select id="role" name="role" required class="form-control lp-input select2">
                                             <option value="">{{__('frontend.select_role')}}</option>
                                             @foreach($roles as $roal)
                                                 <option
@@ -183,22 +183,22 @@
                                 </div>
 
                                 <div class="row form-group">
-                                    <div class="col-md-6">
+                                    <div class="col-md-6 lp-form-group">
                                         <input type="checkbox" id="chk_pass" name="chk_pass" value="yes">{{__('frontend.change_password')}} 
                                     </div>
                                 </div>
                                 <div class="row form-group chk">
 
-                                    <div class="col-md-6">
+                                    <div class="col-md-6 lp-form-group">
                                         <label for="password">{{__('frontend.password')}}  <span class="text-danger">*</span></label>
-                                        <input type="password" id="password" name="password" class="form-control"
+                                        <input type="password" id="password" name="password" class="form-control lp-input"
                                                autocomplete="off">
                                     </div>
-                                    <div class="col-md-6">
+                                    <div class="col-md-6 lp-form-group">
                                         <label for="cnm_password">{{__('frontend.confirm_password')}}<span
                                                 class="text-danger">*</span></label>
                                         <input type="password" id="cnm_password" name="cnm_password"
-                                               class="form-control" autocomplete="off">
+                                               class="form-control lp-input" autocomplete="off">
                                     </div>
                                 </div>
                             </div>
@@ -206,8 +206,8 @@
                             <div class="form-group pull-right">
                                 <div class="col-md-12 col-sm-6 col-xs-12">
                                     <br>
-                                    <a class="btn btn-danger" href="{{ url('admin/client_user') }}">{{__('frontend.cancel')}}</a>
-                                    <button type="submit" class="btn btn-success" id="upload-result"><i
+                                    <a class="btn btn-danger lp-btn lp-btn-danger" href="{{ url('admin/client_user') }}">{{__('frontend.cancel')}}</a>
+                                    <button type="submit" class="btn btn-success lp-btn lp-btn-primary" id="upload-result"><i
                                             class="fa fa-save" id="show_loader"></i>&nbsp;{{__('frontend.save')}}
                                     </button>
                                 </div>
